28b919232c9db93da7f6264b8abd13a8bf73d440
johan9305 <johan9305@hotmail.com>
2014-09-07 16:24:16 -0400

    elimination of yate

--- cgi-bin/list_costumers.py
+++ cgi-bin/list_costumers.py
@@ -1,11 +1,11 @@
 __author__ = 'Joan'
 
 
-import yate
+
 from mako.template import Template
 import Dealing
 clients = Dealing.get_client_list(file = "C:\\Users\\Joan\Desktop\\web\\data\\Clients.txt")
 
-print(yate.start_response())
+
 mytemplate = Template(filename = "C:\\Users\\Joan\\Desktop\\web\\templates\\list.html",enable_loop="True")
 print(mytemplate.render(clients = clients))
