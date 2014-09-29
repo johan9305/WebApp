94d5d3722a7335040ebea7abb549746f32984dc0
johan9305 <johan9305@hotmail.com>
2014-09-29 17:53:56 -0400

    first version of the program done

--- cgi-bin/create_customer.py
+++ cgi-bin/create_customer.py
@@ -3,6 +3,5 @@ import yate
 from mako.template import Template
 temp = Template(filename="C:\\Users\\Joan\\Desktop\\WebApp\\templates\\templ.html")
 print(yate.start_response())
-print(yate.start_form("cgi-bin\save.py"))
 print(temp.render())
-print(yate.end_form("selects"))
+
