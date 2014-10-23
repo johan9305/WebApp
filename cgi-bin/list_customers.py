07fdbff04ad9f53cb0608963bbfafecdf2ce61a2
johan9305 <johan9305@hotmail.com>
2014-10-23 16:29:37 -0400

    database

---
+++ cgi-bin/list_customers.py
@@ -0,0 +1,22 @@
+__author__ = 'Joan'
+
+
+
+from mako.template import Template
+import MySQLdb
+import yate
+
+db = MySQLdb.connect(host= 'localhost',
+                     user="matter_app",
+                      passwd="m@tt3r",
+                      db="matter")
+clients =[]
+cur = db.cursor()
+cur.execute('Select * from Client')
+table = cur.fetchall()
+for a in table:
+    clients.append(a[1])
+
+print(yate.start_response())
+mytemplate = Template(filename ="C:\\Users\\Joan\\Desktop\\WebApp\\templates\\list.html",enable_loop="True")
+print(mytemplate.render(clients = clients))
